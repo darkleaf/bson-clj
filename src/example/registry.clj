abd96ed3227e46ef57a48589561dad2bba0eb9be
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-12-09 22:24:46 +0400

    refactoring

--- src/example/registry.clj
+++ src/example/registry.clj
@@ -2,8 +2,9 @@
   (:import
    (org.bson.codecs.configuration CodecRegistry CodecRegistries)
    (org.bson.codecs.configuration CodecProvider)
-   (org.bson.codecs Codec BsonTypeClassMap BsonTypeCodecMap DecoderContext)
-   (org.bson BsonType BsonReader)
+   (org.bson.codecs Codec BsonTypeClassMap BsonTypeCodecMap
+                    EncoderContext DecoderContext)
+   (org.bson BsonType BsonWriter BsonReader)
 
    (clojure.lang IPersistentMap Sequential)
    (java.util Map)
@@ -37,6 +38,15 @@
    (Map/of BsonType/DOCUMENT IPersistentMap
            BsonType/ARRAY Sequential)))
 
+(defn- write-value [^BsonWriter writer
+                    ^CodecRegistry registry
+                    ^EncoderContext encoderContext
+                    v]
+  (if (nil? v)
+    (.writeNull writer)
+    (let [codec (.get registry (class v))]
+      (.encodeWithChildContext encoderContext codec writer v))))
+
 (defn- read-value [^BsonReader reader
                    ^BsonTypeCodecMap bsonTypeCodecMap
                    ^DecoderContext decoderContext]
@@ -48,6 +58,16 @@
       (let [codec (.get bsonTypeCodecMap bsonType)
             value (.decode codec reader decoderContext)]
         value))))
+
+(defn- write-name [^BsonWriter writer name]
+  (.writeName writer (-> name symbol str)))
+
+(defn- read-name [^BsonReader reader]
+  (keyword (.readName reader)))
+
+(defn- end-of-document? [^BsonReader reader]
+  (= (.readBsonType reader)
+     BsonType/END_OF_DOCUMENT))
 
 (defn persistent-map []
   (let [bsonTypeClassMap (bson-type-class-map)]
@@ -61,23 +81,22 @@
               (encode [_ writer obj encoderContext]
                 (.writeStartDocument writer)
                 (doseq [[k v] obj]
-                  (.writeName writer (-> k symbol str))
-                  (if (nil? v)
-                    (.writeNull writer)
-                    (let [codec (.get registry (class v))]
-                      (.encodeWithChildContext encoderContext codec writer v))));
+                  (write-name writer k)
+                  (write-value writer registry encoderContext v))
                 (.writeEndDocument writer))
               (decode [_ reader decoderContext]
                 (.readStartDocument reader)
-                (let [m (transient {})]
-                  (while (not= (.readBsonType reader)
-                               BsonType/END_OF_DOCUMENT)
-                    (let [fieldName (keyword (.readName reader))
-                          value     (read-value reader bsonTypeCodecMap decoderContext)]
-                      (assoc! m fieldName value)))
-                  (.readEndDocument reader)
-                  (persistent! m))))))))))
-
+                (loop [acc (transient {})]
+                  (if (end-of-document? reader)
+                    (do
+                      (.readEndDocument reader)
+                      (persistent! acc))
+                    (let [field-name (read-name reader)
+                          value      (read-value reader
+                                                 bsonTypeCodecMap
+                                                 decoderContext)
+                          acc        (assoc! acc field-name value)]
+                      (recur acc))))))))))))
 
 (defn persistent-vector []
   (let [bsonTypeClassMap (bson-type-class-map)]
@@ -91,20 +110,20 @@
               (encode [_ writer obj encoderContext]
                 (.writeStartArray writer)
                 (doseq [v obj]
-                  (if (nil? v)
-                    (.writeNull writer)
-                    (let [codec (.get registry (class v))]
-                      (.encodeWithChildContext encoderContext codec writer v))));
+                  (write-value writer registry encoderContext v))
                 (.writeEndArray writer))
               (decode [_ reader decoderContext]
                 (.readStartArray reader)
-                (let [v (transient [])]
-                  (while (not= (.readBsonType reader)
-                               BsonType/END_OF_DOCUMENT)
-                    (let [value (read-value reader bsonTypeCodecMap decoderContext)]
-                      (conj! v value)))
-                  (.readEndArray reader)
-                  (persistent! v))))))))))
+                (loop [acc (transient [])]
+                  (if (end-of-document? reader)
+                    (do
+                      (.readEndArray reader)
+                      (persistent! acc))
+                    (let [value (read-value reader
+                                         bsonTypeCodecMap
+                                         decoderContext)
+                          acc (conj! acc value)]
+                      (recur acc))))))))))))
 
 
 (def ^java.util.List providers
