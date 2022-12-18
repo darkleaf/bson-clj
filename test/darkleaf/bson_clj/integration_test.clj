91ae277f05380d6abcd87bb28172d7c69b09beb7
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-12-18 19:13:51 +0400

    integration test

---
+++ test/darkleaf/bson_clj/integration_test.clj
@@ -0,0 +1,51 @@
+(ns darkleaf.bson-clj.integration-test
+  (:require
+   [clojure.test :as t]
+   [darkleaf.bson-clj.core :as bson])
+  (:import
+   (clojure.lang IPersistentMap)
+   (com.mongodb.client MongoClients)
+   (com.mongodb.client.gridfs GridFSBuckets)
+   (com.mongodb.client.gridfs.model GridFSUploadOptions)
+   (java.io ByteArrayInputStream)
+   (java.util Map)
+   (org.bson.types ObjectId)))
+
+(set! *warn-on-reflection* true)
+
+(def ^String mongo-uri "mongodb://root:example@localhost:27017")
+
+(t/deftest java-map-test
+  (with-open [client (MongoClients/create mongo-uri)]
+    (let [db     (.. client
+                     (getDatabase "test"))
+          coll   (.getCollection db "map-test" Map)
+          id     (ObjectId.)
+          _      (.insertOne coll (Map/of "_id" id "foo" "bar"))
+          filter (bson/->bson (Map/of "_id" id))
+          doc    (.findOneAndDelete coll filter)]
+      (t/is (= "bar" (get doc "foo"))))))
+
+(t/deftest clj-map-test
+  (with-open [client (MongoClients/create mongo-uri)]
+    (let [db     (.. client
+                     (getDatabase "test")
+                     (withCodecRegistry (bson/codec-registry)))
+          coll   (.getCollection db "map-test" IPersistentMap)
+          id     (ObjectId.)
+          _      (.insertOne coll {:_id id :foo "bar"})
+          filter (bson/->bson {:_id id})
+          doc    (.findOneAndDelete coll filter)]
+      (t/is (= "bar" (doc :foo))))))
+
+#_
+(t/deftest grid-fs-test
+  (with-open [client (MongoClients/create mongo-uri)]
+    (let [db     (.. client
+                     (getDatabase "test")
+                     (withCodecRegistry (bson/codec-registry)))
+          bucket (GridFSBuckets/create db)
+          opts   (.. (GridFSUploadOptions.)
+                     (metadata))
+          content (-> "content" (.getBytes) (ByteArrayInputStream.))
+          id      (.uploadFromStream bucket "test.file" content)])))
